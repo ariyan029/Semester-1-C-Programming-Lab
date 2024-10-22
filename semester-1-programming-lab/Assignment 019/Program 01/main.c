4ebc9421426f6378035bf999be18d454bfccc4b3
ariyan029 <murselim49@gmail.com>
2024-10-22 05:35:44 +0000

    p

--- semester-1-programming-lab/Assignment 019/Program 01/main.c
+++ semester-1-programming-lab/Assignment 019/Program 01/main.c
@@ -5,7 +5,7 @@ struct student {int roll ; float cgpa ; char name
 int main (){
     struct student cse [100];
    scanf("%d",&cse[0].roll);
-    cse[o].cgpa=100;
+    cse[0].cgpa=100;
     strcpy(cse[0].name,"ariyan");
     printf("Roll:%d\n",cse[0].roll);
     printf("CGPA:%.2f\n",cse[0].cgpa);
