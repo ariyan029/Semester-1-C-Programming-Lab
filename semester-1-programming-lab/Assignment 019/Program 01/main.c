3bb67705ec85e463b458b642cd546a8f40bec189
ariyan029 <murselim49@gmail.com>
2024-10-29 05:16:34 +0000

    complete push

--- semester-1-programming-lab/Assignment 019/Program 01/main.c
+++ semester-1-programming-lab/Assignment 019/Program 01/main.c
@@ -1,17 +1,61 @@
-#include<stdio.h>
-#include<string.h>
-struct student {int roll ; float cgpa ; char name
-[20];};
-int main (){
-    struct student cse [100];
-   scanf("%d",&cse[0].roll);
-    cse[0].cgpa=100;
-    strcpy(cse[0].name,"ariyan");
-    printf("Roll:%d\n",cse[0].roll);
-    printf("CGPA:%.2f\n",cse[0].cgpa);
-    printf("Name:%s\n",cse[0].name);
+//create a structure named book to store book information like author , tittle , price , .
+//Write a program to insert details for three books . Find the most book and display its info .
+
+#include <stdio.h>
+
+struct Book {
+    char author[50];
+    char title[50];
+    float price;
+};
+
+int main() {
+    struct Book books[3];
+    int i, maxIndex = 0;
+
+    
+    for (i = 0; i < 3; i++) {
+        printf("Enter details for book %d:\n", i + 1);
+        printf("Author: ");
+        scanf("%s", books[i].author);
+        printf("Title: ");
+        scanf("%s", books[i].title);
+        printf("Price: ");
+        scanf("%f", &books[i].price);
+    }
+
+    
+    for (i = 1; i < 3; i++) {
+        if (books[i].price > books[maxIndex].price) {
+            maxIndex = i;
+        }
+    }
+
+    
+    printf("\nThe most expensive book is:\n");
+    printf("Author: %s\n", books[maxIndex].author);
+    printf("Title: %s\n", books[maxIndex].title);
+    printf("Price: %.2f\n", books[maxIndex].price);
+
     return 0;
 }
-    
-    
 
+
+/*------------------OUTPUT---------------------
+Enter details for book 1:
+Author: 1
+Title: 1
+Price: 500
+Enter details for book 2:
+Author: 2
+Title: 2
+Price: 600
+Enter details for book 3:
+Author: 3
+Title: 3
+Price: 70
+
+The most expensive book is:
+Author: 2
+Title: 2
+Price: 600.00*/
